07096e3891d3f37df34aa4c209e9650e7a6e2e8a
SLakshmi333 <nagalakshmi.menthula50@gmail.com>
2022-06-15 17:01:58 +0530

    branch creation

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-	int a,b;
+	int a,b,c;
 	printf("enter the data");
 	scanf("%d%d",&a,&b);
-	printf("%d",a+b);
+	c=a+b;
+	printf("%d",c);
 	return 0;
 }
+
 
 			
